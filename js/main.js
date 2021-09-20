16a5e923cc427401a60addbfc05ea5bd3c7cec52
Viсtoria Kapitonenko <kapitonenko.vick@ya.ru>
2021-09-21 01:25:08 +0300

    feat: add sliders work on mobile

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+const slide = (btns, slider, i) => {
+  const x = i === 0 ? 0 : `calc(-${i * 100}% - 25px)`;
+  slider.style.transform = `translateX(${x})`;
+
+  for (let k = 0; k < btns.length; k++) {
+    btns[k].className = '';
+  }
+  btns[i].className = 'active';
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const btns = document.querySelectorAll('.slides__controls button');
   const slider = document.querySelector('.slides__inner');
@@ -5,13 +15,28 @@ document.addEventListener("DOMContentLoaded", () => {
 
   for (let i = 0; i < btns.length; i++) {
     btns[i].addEventListener('click', () => {
-      const x = i === 0 ? 0 : `calc(-${i * 100}% - 25px)`;
-      slider.style.transform = `translateX(${x})`;
-
-      for (let k = 0; k < btns.length; k++) {
-        btns[k].className = '';
-      }
-      btns[i].className = 'active';
+      slide(btns, slider, i);
+      active = i;
     })
   }
+
+  let touchstartX = 0;
+  let touchendX = 0;
+
+  slider.addEventListener('touchstart', (e) => {
+    touchstartX = e.targetTouches[0].screenX;
+  });
+
+  slider.addEventListener('touchend', (e) => {
+    touchendX = e.changedTouches[0].screenX;
+    const nextSlide = touchendX < touchstartX
+      ? active + 1 === btns.length
+        ? active
+        : active + 1
+      : active - 1 === -1
+        ? 0
+        : active - 1;
+    active = nextSlide;
+    slide(btns, slider, nextSlide);
+  });
 })
